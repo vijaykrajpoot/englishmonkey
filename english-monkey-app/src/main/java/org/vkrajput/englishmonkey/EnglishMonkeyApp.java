8a2c6ae700b65f20739fc5739c4d0c37bc3234b9
Vijay Kumar Rajput <vijay.kumar1@sony.com>
2020-02-20 22:03:00 -0800

    Added POMs

---
+++ english-monkey-app/src/main/java/org/vkrajput/englishmonkey/EnglishMonkeyApp.java
@@ -0,0 +1,15 @@
+package org.vkrajput.englishmonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * EnglishMonkeyApp
+ */
+
+@SpringBootApplication
+public class EnglishMonkeyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishMonkeyApp.class,args);
+    }
+}
---
+++ english-monkey-loader/src/main/java/org/vkrajput/english/monkey/data/dto/Word.java
@@ -0,0 +1,11 @@
+package org.vkrajput.english.monkey.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Word {
+    private String word;
+    private int length;
+}
