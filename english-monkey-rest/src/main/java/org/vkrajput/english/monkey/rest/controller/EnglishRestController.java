6beeb44f0640b6184df8dd80f90670eeb03375e6
Vijay Kumar Rajput <vijay.kumar1@sony.com>
2020-02-22 00:51:52 -0800

    Added Controller

---
+++ english-monkey-rest/src/main/java/org/vkrajput/english/monkey/rest/controller/EnglishRestController.java
@@ -0,0 +1,9 @@
+package org.vkrajput.english.monkey.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnglishRestController {
+
+
+}
